d3a2e5f2c02f39a53874c249fd7cc8e734e72787
BBocho8 <teambbocho@gmail.com>
2024-01-14 15:48:11 +0100

    Single featured home test map fn

--- app/(site)/equipes/[equipe]/actualite/page.tsx
+++ app/(site)/equipes/[equipe]/actualite/page.tsx
@@ -12,32 +12,41 @@ const ActualitePage: FC = async () => {
 			<MainBar menuName="actualite" />
 			<div className="py-8 bg-gray-100 flex flex-col items-center">
 				<div className="hidden md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:px-8 md:gap-x-4 md:gap-y-6">
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
-					<SingleFeaturedPost display="icon" post={posts[0]} />
+					{posts.map((post) => {
+						return (
+							<SingleFeaturedPost key={post._id} display="icon" post={post} />
+						)
+					})}
+					{posts.map((post) => {
+						return (
+							<SingleFeaturedPost key={post._id} display="icon" post={post} />
+						)
+					})}
+					{posts.map((post) => {
+						return (
+							<SingleFeaturedPost key={post._id} display="icon" post={post} />
+						)
+					})}
 				</div>
 				<div className="flex flex-col justify-center items-center px-2 gap-y-3 md:hidden">
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
-					<SingleFeaturedPost display="listIcon" post={posts[0]} />
+					{posts.map((post) => {
+						return (
+							<SingleFeaturedPost
+								key={post._id}
+								display="listIcon"
+								post={post}
+							/>
+						)
+					})}
+					{posts.map((post) => {
+						return (
+							<SingleFeaturedPost
+								key={post._id}
+								display="listIcon"
+								post={post}
+							/>
+						)
+					})}
 				</div>
 				<button className="flex justify-center mt-8 py-2 px-3 rounded border bg-primary uppercase text-white text-xs font-medium hover:bg-opacity-80 ">
 					voir plus
--- app/components/blog/FeaturedPosts.tsx
+++ app/components/blog/FeaturedPosts.tsx
@@ -1,43 +1,21 @@
 import SingleFeaturedPost from "./SingleFeaturedPost"
 
 import { getPosts } from "@/sanity/sanity-utils"
+
 const FeaturedPosts = async () => {
 	const posts = await getPosts()
+
 	return (
 		<div className="grid p-4 gap-y-4 md:grid-cols-2 md:gap-x-4 md:gap-y-12 lg:grid-cols-3  lg:p-0 lg:py-12 lg:px-16">
-			<SingleFeaturedPost display="icon" post={posts[0]} />
-			<SingleFeaturedPost display="icon" post={posts[0]} />
-			<div className="flex flex-col gap-y-4 justify-center">
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-			</div>
-			<div className="flex flex-col gap-y-4 justify-start">
-				{posts.map((post) => {
-					return (
-						<SingleFeaturedPost key={post._id} display="listIcon" post={post} />
-					)
-				})}
-				{/* <SingleFeaturedPost display="listIcon" post={posts[0]} />
-				<SingleFeaturedPost display="listIcon" post={posts[0]} />
-				<SingleFeaturedPost display="listIcon" post={posts[0]} /> */}
-			</div>
-			<SingleFeaturedPost display="icon" post={posts[0]} />
-			<div className="flex flex-col gap-y-4 justify-center">
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-			</div>
-			<div className="flex flex-col gap-y-4 justify-start">
-				<SingleFeaturedPost display="listIcon" post={posts[0]} />
-				<SingleFeaturedPost display="list" post={posts[0]} />
-				<SingleFeaturedPost display="listIcon" post={posts[0]} />
-			</div>
-			<SingleFeaturedPost display="icon" post={posts[0]} />
-
-			<SingleFeaturedPost display="icon" post={posts[0]} />
+			{posts.map((post) => {
+				return <SingleFeaturedPost key={post._id} display="icon" post={post} />
+			})}
+			{posts.map((post) => {
+				return <SingleFeaturedPost key={post._id} display="icon" post={post} />
+			})}
+			{posts.map((post) => {
+				return <SingleFeaturedPost key={post._id} display="icon" post={post} />
+			})}
 		</div>
 	)
 }
