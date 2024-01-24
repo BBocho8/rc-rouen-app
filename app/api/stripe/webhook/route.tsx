06845c724cf9203f9424d7b5eeaa0d79ac773779
BBocho8 <teambbocho@gmail.com>
2024-01-24 17:51:55 +0100

    test webhook

--- app/api/stripe/webhook/route.tsx
+++ app/api/stripe/webhook/route.tsx
@@ -4,24 +4,25 @@ import { NextResponse } from "next/server"
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string)
 
 export async function POST(req: Request) {
-	let event
-	console.log(req.headers.get("stripe-signature") as string)
+	let event: Stripe.Event
 
 	try {
 		// 1. Retrieve the event by verifying the signature using the raw body and secret
-		const rawBody = await req.text()
-		const signature = req.headers.get("stripe-signature") as string
+		const body = await req.text()
+		const signature = req.headers.get("Stripe-Signature") as string
 		// const signature = body.headers["stripe-signature"]
 
 		event = stripe.webhooks.constructEvent(
-			rawBody.toString(),
+			body,
 			signature,
 			process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET as string
 		)
-	} catch (err: any) {
-		console.log(`❌ Error message: ${err.message}`)
-		return NextResponse.json({ error: err.message }, { status: 400 })
+	} catch (error: any) {
+		return NextResponse.json({ error: error.message }, { status: 400 })
+		// return NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
 	}
+
+	const session = event.data.object as Stripe.Checkout.Session
 
 	// Successfully constructed event
 	console.log("✅ Success:", event.id)
@@ -29,6 +30,7 @@ export async function POST(req: Request) {
 	// 2. Handle event type (add business logic here)
 	if (event.type === "checkout.session.completed") {
 		console.log(`💰  Payment received!`)
+		console.log(session)
 	} else {
 		console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`)
 	}
