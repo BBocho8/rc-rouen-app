6050e9a4e348e65ca44c3b4d18abe8882018e98e
BBocho8 <teambbocho@gmail.com>
2024-01-24 16:53:22 +0100

    req.text in webhook

--- app/api/stripe/webhook/route.tsx
+++ app/api/stripe/webhook/route.tsx
@@ -1,15 +1,16 @@
 import Stripe from "stripe"
 import { buffer } from "micro"
+import { NextResponse } from "next/server"
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string)
 
 export async function POST(req: any, res: any) {
 	const body = await req.json()
 	let event
-	console.log(req)
+
 	try {
 		// 1. Retrieve the event by verifying the signature using the raw body and secret
-		const rawBody = await buffer(body)
+		const rawBody = await req.text()
 		const signature = body.headers["stripe-signature"]
 
 		event = stripe.webhooks.constructEvent(
@@ -34,6 +35,5 @@ export async function POST(req: any, res: any) {
 	}
 
 	// 3. Return a response to acknowledge receipt of the event.
-	return res.json({ received: true })
-	// res.json({ received: true })
+	return NextResponse.json({ received: true })
 }
