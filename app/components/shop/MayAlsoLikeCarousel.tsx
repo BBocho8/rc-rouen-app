9b3808721bac32afbfda7cf7478605ccf4e59403
BBocho8 <teambbocho@gmail.com>
2024-01-26 12:30:23 +0100

    Carousel May also like + responsive OK

--- app/components/shop/MayAlsoLikeCarousel.tsx
+++ app/components/shop/MayAlsoLikeCarousel.tsx
@@ -11,8 +11,7 @@ const MayAlsoLikeCarousel = ({ products }: MayAlsoLikeCarouselProps) => {
 	const settings = {
 		dots: false,
 		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
+
 		autoplay: true,
 		speed: 3000,
 		pauseOnHover: true,
@@ -22,18 +21,74 @@ const MayAlsoLikeCarousel = ({ products }: MayAlsoLikeCarouselProps) => {
 		centerMode: true,
 	}
 	return (
-		<div className="pb-12">
-			<h2 className="uppercase text-center mt-16 mb-4 tracking-wide font-medium text-2xl">
-				You may also like
-			</h2>
-			<Slider {...settings}>
-				{products.map((item) => (
-					<div key={item._id} className="">
-						<Product product={item} />
-					</div>
-				))}
-			</Slider>
-		</div>
+		<>
+			<div className=" md:hidden">
+				<h2 className="uppercase text-center mt-16 mb-4 tracking-wide font-medium text-2xl">
+					You may also like
+				</h2>
+				<Slider slidesToShow={2} slidesToScroll={2} {...settings}>
+					{products.map((item) => (
+						<div key={item._id} className="">
+							<Product product={item} />
+						</div>
+					))}
+					{products.map((item) => (
+						<div key={`${item._id} - copy`} className="">
+							<Product product={item} />
+						</div>
+					))}
+					{products.map((item) => (
+						<div key={`${item._id} - copy2`} className="">
+							<Product product={item} />
+						</div>
+					))}
+				</Slider>
+			</div>
+			<div className="pb-12 hidden md:block lg:hidden">
+				<h2 className="uppercase text-center mt-16 mb-4 tracking-wide font-medium text-2xl">
+					You may also like
+				</h2>
+				<Slider slidesToShow={3} slidesToScroll={3} {...settings}>
+					{products.map((item) => (
+						<div key={item._id} className="">
+							<Product product={item} />
+						</div>
+					))}
+					{products.map((item) => (
+						<div key={`${item._id} - copy`} className="">
+							<Product product={item} />
+						</div>
+					))}
+					{products.map((item) => (
+						<div key={`${item._id} - copy2`} className="">
+							<Product product={item} />
+						</div>
+					))}
+				</Slider>
+			</div>
+			<div className="pb-12 hidden lg:block">
+				<h2 className="uppercase text-center mt-16 mb-4 tracking-wide font-medium text-2xl">
+					You may also like
+				</h2>
+				<Slider slidesToShow={4} slidesToScroll={4} {...settings}>
+					{products.map((item) => (
+						<div key={item._id} className="">
+							<Product product={item} />
+						</div>
+					))}
+					{products.map((item) => (
+						<div key={`${item._id} - copy`} className="">
+							<Product product={item} />
+						</div>
+					))}
+					{products.map((item) => (
+						<div key={`${item._id} - copy2`} className="">
+							<Product product={item} />
+						</div>
+					))}
+				</Slider>
+			</div>
+		</>
 	)
 }
 export default MayAlsoLikeCarousel
